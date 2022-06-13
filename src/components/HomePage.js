ca6c1984ea10ace7d9aa7b1e2f7758a0fc707b2d
Данияр Иркагалиев <80203074+DaniyarIrkagaliev@users.noreply.github.com>
2022-06-13 19:37:14 +0300

    displays data from the test json

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,35 +2,28 @@ import { useState, useEffect } from "react";
 import JsonData from "../data/landing-data.json";
 import {Navigation} from "./main/Navigation";
 import {HomeHeader} from "./main/HomeHead";
-import axios from 'axios';
+import JsonProfile from "../data/testUserData.json"
+import {Footer} from "./footer/Footer";
 
-export const Home = () => {
+export const
+    Home = () => {
+
     const [landingPageData, setLandingPageData] = useState({});
     useEffect(() => {
         setLandingPageData(JsonData);
     }, []);
-    const [conversations, setConversations] = useState([]);
-    useEffect(() => {
-        getConversations()
-    }, [])
-    const getConversations = () => {
-        axios.get('https://randomuser.me/api/?results=1').then(response => {
-            let newConversations = response.data.results.map(result => {
-                return {
-                    photo: result.picture.large,
-                    name: `${result.name.first} ${result.name.last}`,
-                };
-            });
-            setConversations([...conversations, ...newConversations])
-        });
-    }
+
 
     return (
-        <div>{conversations.map(conversation =>
-            <Navigation key={conversation.name}
-                        data={conversation}/>)}
+        <div>
+            {JsonProfile.map(profile =>
+            <Navigation
+                key={profile.id}
+                data={profile}/>)}
+
             <HomeHeader data={landingPageData.Header} />
 
+            <Footer data={landingPageData.Contact} />
         </div>
     );
 };
