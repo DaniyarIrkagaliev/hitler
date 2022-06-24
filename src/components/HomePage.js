0f3f4b8fcf4592c0a4ed38933ec36b167424c047
Данияр Иркагалиев <80203074+DaniyarIrkagaliev@users.noreply.github.com>
2022-06-24 20:30:34 +0300

    tabulation

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,31 +1,30 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import JsonData from "../data/landing-data.json";
 import {Navigation} from "./main/Navigation";
 import {HomeHeader} from "./main/HomeHead";
 import JsonProfile from "../data/testUserData.json"
 import {Footer} from "./footer/Footer";
 
-export const
-    Home = () => {
+export const Home = () => {
 
-    const [landingPageData, setLandingPageData] = useState({});
-    useEffect(() => {
-        setLandingPageData(JsonData);
-    }, []);
+        const [landingPageData, setLandingPageData] = useState({});
+        useEffect(() => {
+            setLandingPageData(JsonData);
+        }, []);
 
 
-    return (
-        <div>
-            {JsonProfile.map(profile =>
-            <Navigation
-                key={profile.id}
-                data={profile}/>)}
+        return (
+            <div>
+                {JsonProfile.map(profile =>
+                    <Navigation
+                        key={profile.id}
+                        data={profile}/>)}
 
-            <HomeHeader data={landingPageData.Header} />
+                <HomeHeader data={landingPageData.Header}/>
 
-            <Footer data={landingPageData.Contact} />
-        </div>
-    );
-};
+                <Footer data={landingPageData.Contact}/>
+            </div>
+        );
+    };
 
 export default Home;
